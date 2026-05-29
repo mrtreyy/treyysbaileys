14107e9fd10cc9281df0daa00326c77c0fecdc08
mrtreyy <peacechloe96@gmail.com>
2026-05-29 19:23:47 +0100

    Add start file and node version

---
+++ index.js
@@ -0,0 +1,19 @@
+const { default: makeWASocket, useMultiFileAuthState } = require('./lib')
+
+async function start() {
+    const { state, saveCreds } = await useMultiFileAuthState('./auth')
+    const sock = makeWASocket({
+        auth: state,
+        printQRInTerminal: false,
+        browser: ['AutoFlow', 'Chrome', '1.0.0']
+    })
+    sock.ev.on('creds.update', saveCreds)
+    sock.ev.on('connection.update', async ({ connection, qr }) => {
+        if (qr) {
+            const code = await sock.requestPairingCode('2349033047066')
+            console.log('PAIRING CODE:', code)
+        }
+        if (connection === 'open') console.log('CONNECTED!')
+    })
+}
+start()
